2625bb1cb6677321a4c182db91e87f732dfabd24
Crami <marc.andrada@enti.cat>
2024-07-07 14:30:14 +0200

    DSADSADADSADASSADFSAVDSFASDASEWQEWQ

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    private string menuSceneName;
+    [SerializeField]
+    private string gameSceneName;
+    [SerializeField]
+    private GameObject howToPlayWindow;
+    [SerializeField]
+    private GameObject creditsWindow;
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(menuSceneName);
+    }
+    public void GoToGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void ShowHowToPlay()
+    {
+        howToPlayWindow.SetActive(true);
+    }
+    public void HideHowToPlay()
+    {
+        howToPlayWindow.SetActive(false);
+    }
+   
+    public void ShowCredits()
+    {
+        creditsWindow.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        creditsWindow.SetActive(false);
+    }
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
